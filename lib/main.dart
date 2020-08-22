82a2a9c11c708c30070b82685c0f1c20312e0306
KhineKhineLin <santun2811@gmail.com>
2020-08-22 22:53:53 +0700

    yeah this is first commit

---
+++ lib/main.dart
@@ -0,0 +1,22 @@
+import 'package:flutter/material.dart';
+
+import 'homepage.dart';
+
+void main() {
+  runApp(MyApp());
+}
+
+class MyApp extends StatelessWidget {
+  @override
+  Widget build(BuildContext context) {
+    return MaterialApp(
+      debugShowCheckedModeBanner: false,
+      title: 'Tic Tac Toe Game',
+      theme: ThemeData(
+        primarySwatch: Colors.green
+      ),
+      home: HomePage(),
+    );
+  }
+}
+
